317a0950121f17f3bb0d864702b236c3b9d56008
9HYUN <wosmd1004@naver.com>
2021-12-02 22:56:50 +0900

    Ch2

---
+++ Chapter2/src/ch02/Order.java
@@ -0,0 +1,11 @@
+package ch02;
+
+public class Order {
+
+	int orderId;
+	String buyerId;
+	String sellerId;
+	int productId;
+	String orderDate;
+		
+}
---
+++ Chapter2/src/ch02/Student.java
@@ -0,0 +1,11 @@
+package ch02;
+
+public class Student {
+
+	int studentNumber;
+	String studentName;
+	int majorCode;
+	String majorName;
+	int grade;
+	
+}
