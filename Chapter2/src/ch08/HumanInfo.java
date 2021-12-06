183570594156180c19c2701fe864e46bfa967dad
9HYUN <wosmd1004@naver.com>
2021-12-06 23:22:48 +0900

    homework

--- Chapter2/src/ch08/HumanInfo.java
+++ Chapter2/src/ch08/HumanInfo.java
@@ -1,7 +1,29 @@
 package ch08;
 
-public class HumanInfo {
+public class HumanInfo 
+{
 
+	int userHeight;
+	int userWeight;
+	int userAge;
+	String userSex;
+	String userName;
 	
+	public HumanInfo(int userHeight, int userWeight, int userAge, String userSex, String userName )
+	{
+		this.userHeight = userHeight;
+		this.userWeight = userWeight;
+		this.userAge = userAge;
+		this.userSex = userSex;
+		this.userName = userName;
+	
+	}
+	
+	public String showHunmanInfo()
+	{
+		
+		return "키:" + userHeight  + " 몸무게:" + userWeight + " 성별:" + userSex + " 이름:" + userName + " 나이:" + userAge;
+		
+	}
 	
 }
---
+++ Chapter2/src/ch08/HumanInfoTest.java
@@ -0,0 +1,13 @@
+package ch08;
+
+public class HumanInfoTest {
+
+	public static void main(String[] args) 
+	{
+		HumanInfo human99 = new HumanInfo(180,57,23,"남자","규규");
+		
+		System.out.println(human99.showHunmanInfo());
+		
+	}
+
+}
