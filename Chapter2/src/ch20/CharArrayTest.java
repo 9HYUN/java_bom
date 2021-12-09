e3b299e633e7ddbcbbb0017a3a3cef8cd6e60f26
9HYUN <wosmd1004@naver.com>
2021-12-09 23:37:52 +0900

    array

---
+++ Chapter2/src/ch20/CharArrayTest.java
@@ -0,0 +1,23 @@
+package ch20;
+
+public class CharArrayTest {
+
+	public static void main(String[] args) {
+		char[] alphabets = new char[26];
+		char ch = 'A';
+
+		for(int i = 0; i < alphabets.length; i ++)
+		{
+			alphabets[i] =ch++;
+			//System.out.println(alphabets[i]);
+		}
+		
+		for( char alpha : alphabets)
+		{
+			System.out.println( alpha + ","+ (int)alpha);
+		}
+	}
+
+}
+
+
