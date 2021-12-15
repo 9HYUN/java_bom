f59c0afb8ec08fa386a81b55360a29ef59a68dd0
9HYUN <wosmd1004@naver.com>
2021-12-15 23:36:45 +0900

    Interface

---
+++ Chapter3/src/ch13/domain/userinfo/dao/mysql/UserInfoMySqlDao.java
@@ -0,0 +1,25 @@
+package ch13.domain.userinfo.dao.mysql;
+
+import ch13.domain.userinfo.UserInfo;
+import ch13.domain.userinfo.dao.UserInfoDao;
+
+public class UserInfoMySqlDao implements UserInfoDao
+{
+
+	@Override
+	public void insertUserInfo(UserInfo userInfo) {
+		System.out.println("Insert into MySQL DB userID = " + userInfo.getUserId());
+	}
+
+	@Override
+	public void updateUserInfo(UserInfo userInfo) {
+		System.out.println("Update into MySQL DB userID = " + userInfo.getUserId());
+	}
+
+	@Override
+	public void deleteUserInfo(UserInfo userInfo) {
+		System.out.println("delete into MySQL DB userID = " + userInfo.getUserId());
+		
+	}
+
+}
---
+++ Chapter3/src/ch14/calculatorTest.java
@@ -0,0 +1,24 @@
+package ch14;
+
+public class calculatorTest {
+
+	public static void main(String[] args) 
+	{
+		int num1 = 10;
+		int num2 = 0;
+		
+		
+		Calc calc = new CompleteCalc();
+		System.out.println(calc.add(num1, num2));
+		System.out.println(calc.substract(num1, num2));
+		System.out.println(calc.times(num1, num2));
+		System.out.println(calc.divide(num1, num2));
+		
+		calc.description();
+		
+		int[] arr = {1,2,3,4,5};
+		System.out.println(Calc.total(arr));
+		
+	}
+
+}
